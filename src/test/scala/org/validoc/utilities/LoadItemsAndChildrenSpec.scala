ff83ffa9bbffa35b3ac2165826168f68144b59a2
Phil <phil.rice@iee.org>
2016-02-14 20:57:11 +0000

    Cleaning up

--- src/test/scala/org/validoc/utilities/LoadItemsAndChildrenSpec.scala
+++ src/test/scala/org/validoc/utilities/LoadItemsAndChildrenSpec.scala
@@ -1,12 +1,14 @@
 package org.validoc.utilities
 
 import org.scalatest.{FlatSpec, Matchers}
-
-
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 trait FutureSpec extends FlatSpec with Matchers {
+  def fromFuture[T](f: Future[T]) = Await.result(f, 5 seconds)
+}
+
+trait WebPageAndImageSpec extends FutureSpec {
 
   case class WebPage(imageUrls: List[String])
 
@@ -25,17 +27,38 @@ trait FutureSpec extends FlatSpec with Matchers {
     implicit object ImageAdapter extends IdToItem[Image] {
       def load(id: String): Future[Image] = Future(Image(id))
     }
+
   }
 
-  def fromFuture[T](f: Future[T]) = Await.result(f, 5 seconds)
-
-  val pageaAndChildren = (WebPage(List("ida_1", "ida_2")), List(Image("ida_1"), Image("ida_2")).reverse)
-  val pagebAndChildren = (WebPage(List("idb_1", "idb_2")), List(Image("idb_1"), Image("idb_2")).reverse)
-  val pagecAndChildren = (WebPage(List("idc_1", "idc_2")), List(Image("idc_1"), Image("idc_2")).reverse)
-  val pagedAndChildren = (WebPage(List("idd_1", "idd_2")), List(Image("idd_1"), Image("idd_2")).reverse)
+  val pageaAndChildren = (WebPage(List("ida_1", "ida_2")), List(Image("ida_1"), Image("ida_2")))
+  val pagebAndChildren = (WebPage(List("idb_1", "idb_2")), List(Image("idb_1"), Image("idb_2")))
+  val pagecAndChildren = (WebPage(List("idc_1", "idc_2")), List(Image("idc_1"), Image("idc_2")))
+  val pagedAndChildren = (WebPage(List("idd_1", "idd_2")), List(Image("idd_1"), Image("idd_2")))
 }
 
-class LoadItemsAndChildrenGetSpec extends FutureSpec {
+class FutureHelperSpec extends FutureSpec {
+
+  import ExecutionContext.Implicits.global
+  import FutureHelpers._
+
+  "The FutureHelpers partition method" should "partition a Seq of Future[(String,X)] into a SuccessAndFailureList" in {
+    val e3 = new RuntimeException("e3")
+    val e4 = new RuntimeException("e4")
+    val id1Value = ("id1", Future("value1"))
+    val id2Value = ("id2", Future("value2"))
+    val id3Exception = ("id3", Future.failed(e3))
+    val id4Exception = ("id4", Future.failed(e4))
+    val id5Value5 = ("id5", Future("value5"))
+
+    val SuccessAndFailureSeq(successes, failures) = fromFuture(FutureHelpers.partition(
+      Seq[(String, Future[String])](id1Value, id2Value, id3Exception, id4Exception, id5Value5)))
+    successes shouldBe Seq(("id1", "value1"), ("id2", "value2"), ("id5", "value5"))
+    failures shouldBe Seq(("id3", e3), ("id4", e4))
+  }
+}
+
+
+class LoadItemsAndChildrenGetSpec extends WebPageAndImageSpec {
 
   import DummyWebPageAndImageAdapters._
 
@@ -57,29 +80,30 @@ class LoadItemsAndChildrenGetSpec extends FutureSpec {
 
   "the LoadItemsAndChildren method loadChildren" should "load the children of a webpage" in {
     import LoadItemsAndChildren._
-    fromFuture(loadChildren[WebPage, Image](someWebpage)) shouldBe List(Image("someId_1"), Image("someId_2")).reverse
+    fromFuture(loadChildren[WebPage, Image](someWebpage)) shouldBe List(Image("someId_1"), Image("someId_2"))
   }
 
   "the LoadItemsAndChildren method loadItemAndChildren" should "load the webpage and its  children" in {
     import LoadItemsAndChildren._
-    fromFuture(loadItemAndChildren[WebPage, Image]("id")) shouldBe(WebPage(List("id_1", "id_2")), List(Image("id_1"), Image("id_2")).reverse)
+    fromFuture(loadItemAndChildren[WebPage, Image]("id")) shouldBe(WebPage(List("id_1", "id_2")), List(Image("id_1"), Image("id_2")))
   }
 
 
   "the LoadItemsAndChildren method loadItemsAndChildren" should "load multiple webpages and their  children, putting results in a SuccessAndFailureList" in {
     import LoadItemsAndChildren._
-    val SuccessAndFailureList(successes, failures) = fromFuture(loadItemsAndChildren[WebPage, Image](List("ida", "idb", "idc")))
-    successes shouldBe List(("ida", pageaAndChildren), ("idb", pagebAndChildren), ("idc", pagecAndChildren)).reverse
-    failures shouldBe List()
+    val SuccessAndFailureSeq(successes, failures) = fromFuture(loadItemsAndChildren[WebPage, Image](List("ida", "idb", "idc")))
+    successes shouldBe Seq(("ida", pageaAndChildren), ("idb", pagebAndChildren), ("idc", pagecAndChildren))
+    failures shouldBe Seq()
   }
 }
 
-class LoadItemsAndChildrenGetWithExceptionsSpec extends FutureSpec {
+class LoadItemsAndChildrenGetWithExceptionsSpec extends WebPageAndImageSpec {
 
   import ExecutionContext.Implicits.global
 
   implicit object WebpageAdapter extends IdToItem[WebPage] with ChildIds[WebPage] {
     def load(id: String) = if (id == "idb") Future.failed(new RuntimeException(s"Webpage: $id")) else Future(WebPage(List(id + "_1", id + "_2")))
+
     def getChildIds(t: WebPage) = t.imageUrls
   }
 
@@ -89,10 +113,9 @@ class LoadItemsAndChildrenGetWithExceptionsSpec extends FutureSpec {
 
   "the LoadItemsAndChildren method loadItemsAndChildren" should "separate successes and failures when exception thrown loading webpage" in {
     import LoadItemsAndChildren._
-    val SuccessAndFailureList(successes, failures) = fromFuture(loadItemsAndChildren[WebPage, Image](List("ida", "idb", "idc", "idd")))
-    successes shouldBe List(("ida", pageaAndChildren), ("idd", pagedAndChildren)).reverse
-    failures.map(_._1) shouldBe List("idb", "idc").reverse
-    failures.map(_._2.getMessage) shouldBe List("Webpage: idb", "Image: idc_1").reverse
+    val SuccessAndFailureSeq(successes, failures) = fromFuture(loadItemsAndChildren[WebPage, Image](List("ida", "idb", "idc", "idd")))
+    successes shouldBe List(("ida", pageaAndChildren), ("idd", pagedAndChildren))
+    failures.map(_._1) shouldBe List("idb", "idc")
+    failures.map(_._2.getMessage) shouldBe List("Webpage: idb", "Image: idc_1")
   }
-
 }
